8117f141862dee7f7b59f3e53288e2e4c18e0f60
apla <dot.and.thing@gmail.com>
2011-05-03 10:24:51 +0400

    tidy; uglify preparation

--- WebApp.js
+++ WebApp.js
@@ -6,19 +6,44 @@
 
 // TODO: add support for extjs loading
 
+function MakeEl (name, attributes) {
+	var el = document.createElement (name);
+	if (typeof attributes == 'object') {
+		for (var i in attributes) {
+			el.setAttribute (i, attributes[i]);
+
+			if (i.toLowerCase() == 'class') {
+				el.className = attributes[i];  // for IE compatibility
+
+			} else if (i.toLowerCase() == 'style') {
+				el.style.cssText = attributes[i]; // for IE compatibility
+			}
+		}
+	}
+	for (var i = 2; i<arguments.length; i++) {
+		var val = arguments[i];
+		if (typeof val == 'string')
+			val = document.createTextNode( val );
+		if (el && el.appendChild)
+			el.appendChild (val);
+	}
+	return el;
+}
+
 WebApp = {};
 
 WebApp.Loader = function (config) {
 
 	var self = this;
 	
+	var $document = document;
+	var $window = window;
+	var $addEventListener = 'addEventListener';
+	
 	this.quiet = config.quiet || false;
 	
-	this.messages = config.messages || {loading: 'Loading…', rendering: 'Rendering…'};
+	this.l10n = config.l10n || {loading: 'Loading…', rendering: 'Rendering…'};
 	
-	this.assets = config.assets || config.resources || [];
-	
-	this.tasks = {};
 	this.res = config.res || [];
 	
 	this.tasks = new function () {
@@ -58,14 +83,14 @@ WebApp.Loader = function (config) {
 	this.isLoading = false;
 	this.loadMore  = false;
 	this.isDone    = false;
-	// if (window.addEventListener) 
-	//window.addEventListener ("error", function (eee) {
+	// if ($window[$addEventListener]) 
+	//$window[$addEventListener] ("error", function (eee) {
 		//console.error (eee);
 		// self.completed ();
 	// }, false);
 
 	this.begin = function () {
-		this.progress = new WebApp.Loader.Progress (this.messages);
+		this.progress = new WebApp.Loader.Progress ({l10n: this.l10n});
 		
 		if (!this.quiet)
 			this.progress.show ();
@@ -163,17 +188,17 @@ WebApp.Loader = function (config) {
 	
 	// offline cache handling
 	
-	if (window.applicationCache) {
+	if ($window.applicationCache) {
 		
-		cache = window.applicationCache;
+		var cache = $window.applicationCache;
 		
-		updateCacheStatus = function () {
+		var updateCacheStatus = function () {
 			if (cache.status == 4) {
 				cache.swapCache ();
 			}
 		}
 
-		updateCacheError = function (e) {
+		var updateCacheError = function (e) {
 //			console.log(e, cacheStates[cache.status]);
 		}
 
@@ -182,10 +207,10 @@ WebApp.Loader = function (config) {
 		var i = events.length;
 
 		while (i--) {
-			cache.addEventListener(events[i], updateCacheStatus, false);
+			cache[$addEventListener](events[i], updateCacheStatus, false);
 		}
 		
-		cache.addEventListener('error', updateCacheError, false);
+		cache[$addEventListener]('error', updateCacheError, false);
 	}
 	
 	//flag to indicate whether target function has already been run
@@ -193,43 +218,49 @@ WebApp.Loader = function (config) {
 	
 	WebApp.Loader.instance = this;
 	
-	if (document.addEventListener) {
-		document.addEventListener("DOMContentLoaded", function() {
+	if ($document[$addEventListener]) {
+		$document[$addEventListener]("DOMContentLoaded", function() {
 			WebApp.Loader.ready ++;
-			if (WebApp.Loader.ready == 1) WebApp.Loader.instance.begin()
+			if (WebApp.Loader.ready == 1) self.begin()
 			window.onload = null;
 		}, false)
-	} else if (document.all && !window.opera) {
-		document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"><\/script>')
-		var contentloadtag = document.getElementById ("contentloadtag")
+	} else if ($document.all && !window.opera) {
+		$document.write('<script type="text/javascript" id="contentloadtag" defer="defer" src="javascript:void(0)"><\/script>')
+		var contentloadtag = $document.getElementById ("contentloadtag")
 		contentloadtag.onreadystatechange = function(){
 			if (self.readyState == "complete"){
 				WebApp.Loader.ready ++;
-				if (!WebApp.Loader.ready == 1) WebApp.Loader.instance.begin()
+				if (!WebApp.Loader.ready == 1) self.begin()
 				window.onload = null;
 			}
 		}
 	}
 
-	window.onload = function () {
+	$window.onload = function () {
 		setTimeout("if (!WebApp.Loader.ready) WebApp.Loader.instance.begin()", 0)
 	}
 	
 }
 
 WebApp.Loader.Task = function (url) {
+
+	var $a = arguments;
+	var $document = document;
+	var $window = window;
+	var $addEventListener = 'addEventListener';
+	var $querySelector = 'querySelector';
 	
-	this.url       = arguments[0];
-	
+	this.url       = $a[0];
+
 	// parameters parse:
-	if (arguments.length == 2 && arguments[1] instanceof Object) {
-		this.require   = arguments[1].require;
-		this.type      = arguments[1].type;
-		this.forRender = arguments[1].forRender;
+	if ($a.length == 2 && $a[1] instanceof Object) {
+		this.require   = $a[1].require;
+		this.type      = $a[1].type;
+		this.forRender = $a[1].forRender;
 	} else {
-		this.require   = arguments[1];
-		this.type      = arguments[2];
-		this.forRender = arguments[3];
+		this.require   = $a[1];
+		this.type      = $a[2];
+		this.forRender = $a[3];
 	}
 	
 	if (!this.type)
@@ -300,15 +331,15 @@ WebApp.Loader.Task = function (url) {
 		
 		this.state = 2;
 		
-		var node = document.createElement(this.nodeName);
+		var node = MakeEl (this.nodeName, this.nodeAttrs);
 		
 		//console.log (nodeName);
 		// we add events for any browser who supports events
-		if (node.addEventListener) {
-			node.addEventListener("load", function () {
+		if (node[$addEventListener]) {
+			node[$addEventListener]("load", function () {
 				self.completed ();
 			}, false);
-			node.addEventListener("error", function (event) {
+			node[$addEventListener]("error", function (event) {
 				console.log (event);
 			}, false);
 
@@ -323,15 +354,9 @@ WebApp.Loader.Task = function (url) {
 			
 			// TODO: use solution from http://www.backalleycoder.com/2011/03/20/link-tag-css-stylesheet-load-event/:
 			var loadCSS = function(url, callback){
-				var link = document.createElement('link');
-				link.type = 'text/css';
-				link.rel = 'stylesheet';
-				link.href = url;
 
-				document.getElementsByTagName('head')[0].appendChild(link);
-
-				var html = document.getElementsByTagName('html')[0];
-				var img = document.createElement('img');
+				var html = $document[$querySelector]('html')[0];
+				var img = MakeEl ('img');
 				img.onerror = function(){
 					if(callback) callback(link);
 					html.removeChild(img);
@@ -372,96 +397,101 @@ WebApp.Loader.Task = function (url) {
 			}, 50)
 		}
 		
-		for (var k in this.nodeAttrs) {
-			node.setAttribute (k, this.nodeAttrs[k]);
-		}
-		// node.src = src;
-		document.getElementsByTagName("head").item(0).appendChild(node);
+		$document[$querySelector]("head").appendChild(node);
 
 	}
 				
 }
 
-WebApp.Loader.Progress = function (messages) {
+WebApp.Loader.Progress = function (config) {
 	
 	var self = this;
 	
-	this.messages = messages;
+	var $document = document;
+	var $window = window;
+	var $addEventListener = 'addEventListener';
+	var $innerHeight = 'innerHeight';
+	var $innerWidth  = 'innerWidth';
+	
+	this.l10n = config.l10n;
 	
 	this.show = function () {
-		// var b = document.body;
+		var body      = $document.body;
+		var bodyStyle = body.style;
 		
 		// var node = document.createElement(this.nodeName);
-		var body = document.getElementsByTagName("body").item(0);
+		//var body = $document.getElementsByTagName("body").item(0);
 		
-		body.style.width  = window.innerWidth + 'px';
-		body.style.height = window.innerHeight + 'px';
+		bodyStyle.width  = $window[$innerWidth] + 'px';
+		bodyStyle.height = $window[$innerHeight] + 'px';
 		
-		// console.log (Math.floor(window.innerHeight/2));
+		// console.log (~~(window[$innerHeight]/2));
 		
 		//if (resources[type+'-'+version])
-		var node = document.createElement ('div');
-		node.setAttribute ('id', 'web-app-progress');
-		node.setAttribute ('style', 'font-family: "Helvetica Neue",HelveticaNeue,"Helvetica-Neue",Helvetica,sans-serif; -webkit-transition: opacity linear 2s; -moz-transition: opacity linear 2s; text-align: center; padding: 10px; border-radius: 10px; background: rgba(192, 192, 192, 0.3); height: 50px; width: 300px; text-align: left; position: absolute !important; z-index: 100; top: '+(Math.floor(window.innerHeight/2))+'px; left: 50%; margin: -25px 0 0 -150px; display: block;');
+		var node = MakeEl ('div', {
+			id: 'web-app-progress',
+			style: 'font-family: "Helvetica Neue",HelveticaNeue,"Helvetica-Neue",Helvetica,sans-serif; -webkit-transition: opacity linear 2s; -moz-transition: opacity linear 2s; text-align: center; padding: 10px; border-radius: 10px; background: rgba(192, 192, 192, 0.3); height: 50px; width: 300px; text-align: left; position: absolute !important; z-index: 100; top: '+(~~(window[$innerHeight]/2))+'px; left: 50%; margin: -25px 0 0 -150px; display: block;'
+		}, this.l10n.loading);
+		
 		body.insertBefore(node, body.firstChild);
 
-		var nodeText = document.createTextNode (this.messages.loading);
-		//nodeText.setAttribute ('class', '');
-		//nodeText.setAttribute ('style', 'background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, color-stop(0%, #7fb6e7), color-stop(2%, #2986d6), color-stop(100%, #1d5e96)); -webkit-transition: width linear 0.1s; height: 20px; width: 0; display: "block";');
-		node.appendChild (nodeText);
+		var nodeProgress = MakeEl ('div', {
+			'class': '',
+			style: 'background: -moz-linear-gradient(top,  #2986d6,  #1d5e96); background: -webkit-gradient(linear, 0% 0%, 0% 100%, color-stop(0%, #7fb6e7), color-stop(2%, #2986d6), color-stop(100%, #1d5e96)); -moz-transition: width linear 0.3s; -webkit-transition: width linear 0.3s; height: 8px; border-radius: 5px; width: 0; display: block;'
+		});
 		
-		this.nodeText = nodeText;
-		
-		var nodeProgress = document.createElement ('div');
-		nodeProgress.setAttribute ('class', '');
-		nodeProgress.setAttribute ('style', 'background: -moz-linear-gradient(top,  #2986d6,  #1d5e96); background: -webkit-gradient(linear, 0% 0%, 0% 100%, color-stop(0%, #7fb6e7), color-stop(2%, #2986d6), color-stop(100%, #1d5e96)); -moz-transition: width linear 0.3s; -webkit-transition: width linear 0.3s; height: 8px; border-radius: 5px; width: 0; display: block;');
 		node.appendChild (nodeProgress);
 		
 		node.style.display = 'none';
 		node.style.display = 'block';
 
 		body.scrollTop = body.scrollHeight;
-		body.style.width  = window.innerWidth + 'px';
-		body.style.height = window.innerHeight + 'px';
+		bodyStyle.width  = $window[$innerWidth] + 'px';
+		bodyStyle.height = $window[$innerHeight] + 'px';
 
 		// nodeProgress.style.width = '1px';
-		this.containerNode = node;
-		this.progressBarNode = nodeProgress;
+		this.boxNode = node;
+		this.barNode = nodeProgress;
 		
 		this.scrollChecker = setInterval (function (){
 			//console.log ('pageYOffset: ' + window.pageYOffset);
-			if (window.pageYOffset > 0) {
+			if ($window.pageYOffset > 0) {
 				self.onReflow();
 			}
 		}, 50);
 	}
 	
 	this.update = function (progress) {
-		this.progressBarNode.style.width = ~~ (progress * 100) + '%';
+		this.barNode.style.width = ~~ (progress * 100) + '%';
 		
 		if (progress == 1) {
-			this.containerNode.firstChild.textContent = this.messages.rendering;
+			this.boxNode.firstChild.textContent = this.l10n.rendering;
 		}
 	}
 	
 	this.onReflow = function () {
-		if (Math.floor(window.innerHeight/2) + window.pageYOffset != self.containerNode.offsetTop){
-			this.containerNode.style.top  = Math.floor(window.innerHeight/2) + window.pageYOffset + 'px'
-			this.containerNode.style.left = Math.floor(window.innerWidth/2)  + window.pageXOffset + 'px'
+		if (~~($window[$innerHeight]/2) + $window.pageYOffset != self.boxNode.offsetTop){
+			this.boxNode.style.top  = ~~($window[$innerHeight]/2) + $window.pageYOffset + 'px'
+			this.boxNode.style.left = ~~($window[$innerWidth]/2)  + $window.pageXOffset + 'px'
 		}
-		//console.log ('iH: ' + Math.floor(window.innerHeight/2) + ' pageYOffset: ' + window.pageYOffset);
-		//console.log ('offsetTop: ' + self.containerNode.offsetTop);
+		
+		console.log ($window.offsetHeight, $window[$innerHeight], $window.clientHeight, $window);
+		
+		//console.log ('iH: ' + ~~(window[$innerHeight]/2) + ' pageYOffset: ' + window.pageYOffset);
+		//console.log ('offsetTop: ' + self.boxNode.offsetTop);
 
 	}
 	
 	this.hide = function () {
-		this.containerNode.style.opacity = 0;
-		this.containerNode.addEventListener ('webkitTransitionEnd', this.afterHide, false);
+//		console.log ('!!!!!!!!!!!!!!');
+		this.boxNode.style.opacity = 0;
+		this.boxNode[$addEventListener]('webkitTransitionEnd', this.afterHide, false);
 		//clearInterval(this.scrollChecker);
 	};
 	
 	this.afterHide = function () {
-		clearInterval(this.scrollChecker);
+//		console.log ('??????????????????');
+		clearInterval(self.scrollChecker);
 	}
 
 
